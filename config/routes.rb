f9988f3172565d5fc55cdb0f655f1232a8f364ed
Renato Franco <renato_ny@live.com>
2024-03-27 20:15:25 -0300

    fix: fixing home controller on dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,43 @@
 
 Rails.application.routes.draw do
 
-  # Dashboard
-  mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
+  # Admin
+  # https://admin.rubypolsce.pl
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   # Authentication
+  # https://auth.rubypolsce.pl
   devise_for :users
 
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Application
+  # https://rubypolsce.pl
   root "home#index"
   get 'analytics' => 'donations#analytics'
   resources :donations, only: [:index, :create]
   resources :newsletters, only: [:create, :destroy], param: :token
 
-  # Namespace
+  # Dashboard
+  # https://dashboard.rubypolsce.pl
   namespace :dashboard do
     get '', to: 'home#index'
+  end
+
+  # API
+  # https://api.rubypolsce.pl/
+  namespace :api do
+    namespace :v1 do
+      defaults format: :json do
+        # TODO: some json from here
+      end
+    end
+
+    namespace :v2 do
+      defaults format: :xml do
+        # TODO: some xml from here
+      end
+    end
   end
 end
